03d0b547eaeab545b552a0a91cfcfa20a4da823d
Byron <byronk5@gmail.com>
2022-04-20 22:25:44 +0200

    Added loading gif

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,31 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import FeedbackData from '../data/Feedbackdata';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState(FeedbackData);
+    const [isLoading, setisLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
 
     const [feedbackEditObject, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     });
+
+    useEffect(() => {
+        fetchFeedback();
+    }, []);
+
+    // Fetch Feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(
+            'http://localhost:5000/feedback?_sort=id&_order=desc'
+        );
+        const data = await response.json();
+
+        setFeedback(data);
+        setisLoading(false);
+    };
 
     // Delete feedback
     const deleteFeedback = (id, rating) => {
@@ -50,6 +65,7 @@ export const FeedbackProvider = ({ children }) => {
             value={{
                 feedback,
                 feedbackEditObject,
+                isLoading,
                 deleteFeedback,
                 addFeedback,
                 editFeedback,
